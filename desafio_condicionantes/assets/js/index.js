7dc903a5892d62986a9fae1749471ba63116b0e4
hefox83 <151103283+hefox83@users.noreply.github.com>
2024-03-09 09:33:09 -0300

    condicionantes

---
+++ desafio_condicionantes/assets/js/index.js
@@ -0,0 +1,9 @@
+let btnboton = document.querySelector('#botonborde');
+let img = document.querySelector ('#image1');
+
+if( img.style.border != '5px solid black'){
+    img.style.border = '5px solid black'
+}
+else{
+    img.style.borderSolid = 'none'
+}
